03b13e3010b5ef2204eef0702262c331c416b216
tetehoma <tetehoma@gmail.com>
2022-07-11 10:51:03 -0300

    Criando Calculadora

---
+++ src/com/dio/Calculadora.java
@@ -0,0 +1,46 @@
+package com.dio;
+
+import java.util.Scanner;
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner (System.in);
+		
+		int a;
+		int b;
+		
+		System.out.println("Digite o primeiro valor:");
+		a = scan.nextInt();
+		System.out.println("Digite o segundo valor:");
+		b = scan.nextInt();
+		
+		int soma = soma(a,b);
+		int subtracao = subtracao(a,b);
+		int multiplicacao = multiplicacao(a,b);
+		float divisao = divisao(a,b);
+		
+		System.out.println("soma" + soma);
+		System.out.println("subtracao" + subtracao);
+		System.out.println("multiplicacao" + multiplicacao);
+		System.out.println("divisao" + divisao);
+		
+	}
+
+	public static int soma (int a, int b) {
+		return a + b;		
+	}
+
+	public static int subtracao(int a, int b) {
+		return a - b;		
+	}
+
+	public static float divisao(float a, float b) {
+		return a / b;		
+	}
+
+	public static int multiplicacao(int a, int b) {
+		return a * b;		
+	}
+
+}
