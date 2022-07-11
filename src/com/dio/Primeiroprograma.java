64ee72a0dc7600a5ce77967b5bbf9a368a9d32c2
tetehoma <tetehoma@gmail.com>
2022-07-11 08:59:12 -0300

    First commit

---
+++ src/com/dio/Primeiroprograma.java
@@ -0,0 +1,23 @@
+package com.dio;
+
+import com.dio.model.Gato;
+
+public class Primeiroprograma {
+
+	public static void main(String[] args) {
+		/* int a = 2;
+		int b = 3;
+		System.out.println("Hello World" + (a+b));*/
+		
+		Gato gato = new Gato(); 
+		Livros livros = new Livros();
+		
+		System.out.println(gato);
+		System.out.println(livros);
+	}
+}
+
+class Livros {
+		private String nome;
+		private String npag;
+}
